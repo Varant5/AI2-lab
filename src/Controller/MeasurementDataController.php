98b67e3748b741d08bd3980277d2a98f47f5c950
Varant5 <neroo555@wp.pl>
2024-10-23 00:44:48 +0200

    lab-e done

--- src/Controller/MeasurementDataController.php
+++ src/Controller/MeasurementDataController.php
@@ -10,11 +10,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/measurement/data')]
 class MeasurementDataController extends AbstractController
 {
     #[Route('/', name: 'app_measurement_data_index', methods: ['GET'])]
+    #[IsGranted('ROLE_MEASUREMENTDATA_INDEX')]
     public function index(MeasurementDataRepository $measurementDataRepository): Response
     {
         return $this->render('measurement_data/index.html.twig', [
@@ -23,6 +25,7 @@ class MeasurementDataController extends AbstractController
     }
 
     #[Route('/new', name: 'app_measurement_data_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_MEASUREMENTDATA_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $measurementDatum = new MeasurementData();
@@ -45,6 +48,7 @@ class MeasurementDataController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_measurement_data_show', methods: ['GET'])]
+    #[IsGranted('ROLE_MEASUREMENTDATA_SHOW')]
     public function show(MeasurementData $measurementDatum): Response
     {
         return $this->render('measurement_data/show.html.twig', [
@@ -53,6 +57,7 @@ class MeasurementDataController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_measurement_data_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_MEASUREMENTDATA_EDIT')]
     public function edit(Request $request, MeasurementData $measurementDatum, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(MeasurementDataType::class, $measurementDatum, [
@@ -73,6 +78,7 @@ class MeasurementDataController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_measurement_data_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_MEASUREMENTDATA_DELETE')]
     public function delete(Request $request, MeasurementData $measurementDatum, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$measurementDatum->getId(), $request->request->get('_token'))) {
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                ],
+                'multiple' => true,   // This ensures multiple values can be selected
+                'expanded' => false,  // Set to false for a dropdown list, true for checkboxes
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
